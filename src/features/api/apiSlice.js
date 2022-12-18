fa62de1c725596ec749539cefe2b35ea8d5043d9
Rasujon3 <rasujon3@gmail.com>
2022-12-18 17:13:15 +0600

    9.7 Authenticated Request

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,6 +4,13 @@ export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.REACT_APP_API_URL || "http://localhost:9000",
+    prepareHeaders: async (headers, { getState, endpoint }) => {
+      const token = getState()?.auth?.accessToken;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
   }),
   tagTypes: [],
   endpoints: (builder) => ({}),
